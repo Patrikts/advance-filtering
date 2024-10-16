0b406661cf71597dc370808e324d00608b3b2809
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-10-16 12:38:48 -0300

    edit Price

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,5 +1,33 @@
 import "./Price.css";
 
-export default function Price() {
-  return <div>Price</div>
+function Price() {
+  return (
+  <div className="ml">
+    <h2 className="sidebar-title price-title">Price</h2>
+
+    <label className='side-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>All
+      </label>
+      <label className='side-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>$0 - $50
+      </label>
+      <label className='side-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>$50 - $100
+      </label>
+      <label className='side-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>$100 - $150
+      </label>
+      <label className='side-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>Over $150
+      </label>
+
+  </div>
+  );
 }
+
+export default Price;
