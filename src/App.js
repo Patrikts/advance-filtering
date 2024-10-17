9a684b79939349552d37399f22a8e3a677fe0ffa
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-10-17 15:35:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import products from './db/data'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null)
+  const [query, setQuery] = useState("")
 
   // ---------- Input Filter ---------- 
-  const [query, setQuery] = useState("")
 
   const handleInputChange = event => {
     setQuery(event.target.value)
@@ -27,8 +27,46 @@ function App() {
     setSelectedCategory(event.target.value)
   }
 
+  // ---------- Buttons Filter ---------- 
+  const hadleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
 
-  return (
+  function filteredData(products, selected ,query) {
+    let filteredProducts = products
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+    // Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+      ({ category, color, company, newPrice, title }) => 
+      category === selected ||
+      color === selected ||
+      company === selected ||
+      newPrice == selected ||
+      title === selected
+    );
+  }   
+  
+  return filteredProducts.map(({img, title, star, reviews, prevPrice}) => (
+    <Card 
+    key={Math.random()}
+    img={img}
+    title={title}
+    star={star}
+    reviews={reviews}
+    newPrice={newPrice}
+    prevPrice={prevPrice}
+    />
+  ))
+
+}
+
+return (
     <>
       <Sidebar />
       <Navigation />
