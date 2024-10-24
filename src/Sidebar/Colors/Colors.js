678fcc24b52d460fe4513e39e30f8b6fb37988c8
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-10-24 14:55:13 -0300

    update Colors

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,14 +1,50 @@
 import Input from "../../components/Input";
 import "./Colors.css";
 
-function Colors() {
+function Colors({handleChange}) {
   return (
     <div>
     <h2 className="sidebar-title-color-title">Colors</h2>
-    <Input />
-    <Input />
-    <Input />
-    <Input />
+    <label className='sidebar-label-container'>
+      <input onChange={handleChange} type='radio' value="" name='test2'/>
+      <span className='checkmark'></span>All
+    </label>
+
+<input 
+  handleChange={handleChange}
+  value="black"
+  title="Black"
+  name="test1"
+  color="black"
+/>
+<input 
+  handleChange={handleChange}
+  value="blue"
+  title="Blue"
+  name="test1"
+  color="blue"
+/>
+<input 
+  handleChange={handleChange}
+  value="red"
+  title="Red"
+  name="test1"
+  color="red"
+/>
+<input 
+  handleChange={handleChange}
+  value="green"
+  title="Green"
+  name="test1"
+  color="green"
+/>
+
+<label className="sidebar-label-container">
+
+<input type="radio" onChange={handleChange} value="white" name="test1"/>
+
+</label>
+
     </div>
   );
 }
--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -7,6 +7,36 @@ function Price(handleChange) {
   <div className="ml">
     <h2 className="sidebar-title price-title">Price</h2>
 
+    <label className='sidebar-label-container'>
+      <input onChange={handleChange} type='radio' value="" name='test2'/>
+      <span className='checkmark'></span>All
+    </label>
+
+<input 
+  handleChange={handleChange}
+  value={50}
+  title="$0 - 50"
+  name="test2"
+/>
+<input 
+  handleChange={handleChange}
+  value={100}
+  title="$50 - 100"
+  name="test2"
+/>
+<input 
+  handleChange={handleChange}
+  value={150}
+  title="$100 - 150"
+  name="test2"
+/>
+<input 
+  handleChange={handleChange}
+  value={200}
+  title="Over $150"
+  name="test2"
+/>
+
   </div>
   );
 }
